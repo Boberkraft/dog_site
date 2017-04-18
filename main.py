d41e70d5d993920934263dc6041f0021de60d0da
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2017-04-18 22:17:16 +0200

    comments in pl

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 from flask import Flask, render_template, request
-
 app = Flask(__name__, static_folder='files', static_url_path='')
 
 @app.route('/')
@@ -15,25 +14,39 @@ def order():
 @app.route('/zamowienia', methods=['POST', 'GET'])
 def orders():
     if request.form:
-        add_to_base(request.form)
+        #  ktoś przysłał formularz. Pewnie to są zamówienia
+        add_to_base(request.form)  # zapisz w pliku
 
-    with open('orders', encoding='utf-8') as ff:
-        orders = []
-        for line in ff:
-            line = line.split('|X|')
-            orders.append(dict(name=line[0], order=line[3]))
+    orders = get_orders()  # odczytaj zamówienia z pliku
 
-    return render_template('orders.html', orders=orders)
+    return render_template('orders.html', orders=orders)  # zrenderuj stronkę
 
 
 def add_to_base(form):
+    """Dodaje zamówienie do pliku"""
+    # pobiera z requestu niezbędne rzeczy.
+    # Jeżeli jakiegoś nie będzie to trudno
     name = form['name']
     email = form['email']
     phone = form['phone']
     order = form['order']
 
+    # otwórz plik orders w trybie append 'a' czyli dodawaj na końcu pliku
     with open('orders', 'a', encoding='utf-8') as ff:
-        ff.write('{}|X|{}|X|{}|X|{}\n'.format(name, email, phone, order))
+        # wygeneruj wiersz do zapisania
+        temp = '{}|X|{}|X|{}|X|{}'.format(name, email, phone, order)
+        ff.write(temp + '\n')
+
+def get_orders():
+    """Wczytuje zamówienia z pliku"""
+    # otwórz plik o nazwie orders
+    with open('orders', encoding='utf-8') as ff:
+        orders = []
+        for line in ff:
+            # dla każdej linii w pliku
+            line = line.split('|X|')  # rozerwij wiersz w  miejscach '|X|'
+            orders.append(dict(name=line[0], order=line[3]))  # zachowaj tylko 1 i 4 część wiersza
+    return orders  # zwróc zamówienia
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.17')
+    app.run()
